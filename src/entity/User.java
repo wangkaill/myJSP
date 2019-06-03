65c6678df67cfcf83aaf21f9c18253da25b3c660
wangkaill <2240649813@qq.com>
2019-06-03 22:51:37 +0800

    第一次上传

---
+++ src/entity/User.java
@@ -0,0 +1,49 @@
+package entity;
+
+public class User {
+    private String userid;
+    private String uname;
+    private String password;
+    private String email;
+    private String lasttime;
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLasttime() {
+        return lasttime;
+    }
+
+    public void setLasttime(String lasttime) {
+        this.lasttime = lasttime;
+    }
+}
---
+++ web/jsp/www.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 汪凯
+  Date: 2019/6/3
+  Time: 21:45
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+    22222
+</head>
+<body>
+
+</body>
+</html>
